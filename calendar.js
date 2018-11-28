eae8bf81280ff9f8cd117ade4a8289b80666092c
Vladimir <shues@bk.ru>
2018-11-28 10:58:24 +0300

    new commit

--- calendar.js
+++ calendar.js
@@ -26,9 +26,10 @@ function load_table(par,elem,par_element){
         var local_week = document.createElement('tr');
         for(var i=0; i<=6; i++){
             var local_day = document.createElement('td');
+            local_day.onclick = set_this_current;
             if (i==day_of_week){
                 local_day.textContent = local_date.getDate();
-                if ((local_date.getDate == now.getDate) & (par==0)){
+                if ((local_date.getDate() == now.getDate()) & (par==0)){
                     local_day.className = 'current';
                 } 
                 local_date.setDate(local_date.getDate()+1);
@@ -48,7 +49,17 @@ function load_table(par,elem,par_element){
 
 function create_calendar_window(){
     //заполним таблицы
-    for(var month_counter = -2; month_counter <= 2; month_counter++){
+    for(var month_counter = -1; month_counter <= 2; month_counter++){
         load_table(month_counter,'month_table','calendar_panel');
     }
+}
+
+function set_this_current(){
+    var mass_of_old_current_el = document.getElementsByClassName('current');
+    for(var i=mass_of_old_current_el.length-1; i>=0; i--){
+        mass_of_old_current_el[i].className = '';
+    }
+    this.className = 'current';
+    this.parentElement.parentElement.parentElement.children[0].className = 'current';
+    
 }
---
+++ chack.js
@@ -0,0 +1,4 @@
+function change_contragent(par){
+    var contragents = send_request('get_contragent_spis');
+    
+}
